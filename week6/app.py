c3f1df1cbbc1df00f02cb904ad4a16e874d1ad70
NallaSandeep <nallasandeep056@gmail.com>
2022-02-08 20:01:25 +0530

    implemented Enrollment end points

--- week6/app.py
+++ week6/app.py
@@ -40,11 +40,11 @@ class Course(db.Model):
     course_description = db.Column(db.String)
 
 
-class Enrollments(db.Model):
-    __tablename__ = 'enrollments'
+class Enrollment(db.Model):
+    __tablename__ = 'enrollment'
     enrollment_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    estudent_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
-    ecourse_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable=False)
 
 
 course_fields = {
@@ -61,6 +61,16 @@ student_fields = {
     'roll_number': fields.String
 }
 
+enrollment_fields = {
+    'enrollment_id': fields.Integer,
+    'student_id': fields.Integer,
+    'course_id': fields.Integer
+}
+
+enrollment_list_fields = {
+    fields.List(fields.Nested(enrollment_fields))
+}
+
 create_course_parser = reqparse.RequestParser()
 create_course_parser.add_argument('course_name')
 create_course_parser.add_argument('course_code')
@@ -70,6 +80,11 @@ create_student_parser = reqparse.RequestParser()
 create_student_parser.add_argument('first_name')
 create_student_parser.add_argument('last_name')
 create_student_parser.add_argument('roll_number')
+
+create_enrollment_parser = reqparse.RequestParser()
+create_enrollment_parser.add_argument('enrollment_id')
+create_enrollment_parser.add_argument('student_id')
+create_enrollment_parser.add_argument('course_id')
 
 
 class BusinessValidationError(HTTPException):
@@ -83,13 +98,11 @@ class InputValidationError(HTTPException):
         self.response = make_response(error_message, status_code)
 
 
-# String validation yet to be done
 class CourseApi(Resource):
 
     @marshal_with(course_fields)
     def get(self, course_id):
         user = db.session.query(Course).filter(Course.course_id == course_id).first()
-        # print(user)
         if user:
             return user
         else:
@@ -101,12 +114,11 @@ class CourseApi(Resource):
         course_name = args.get('course_name', None)
         course_code = args.get('course_code', None)
         course_description = args.get('course_description', None)
-        if course_name is None:
+        if course_name is None or course_name.isnumeric():
             raise BusinessValidationError(400, 'COURSE001', 'Course Name is required and should be string.')
-        if course_code is None:
+        if course_code is None or course_code.isnumeric():
             raise BusinessValidationError(400, 'COURSE002', 'Course Code is required and should be string.')
-        print(not isinstance(course_description, str))
-        if not isinstance(course_description, str):
+        if course_description.isnumeric():
             raise BusinessValidationError(400, 'COURSE003', 'Course Description should be string.')
         user_by_course_id = db.session.query(Course).filter(Course.course_id == course_id).first()
         if user_by_course_id is None:
@@ -123,6 +135,10 @@ class CourseApi(Resource):
         if user:
             db.session.delete(user)
             db.session.commit()
+            enrollment = db.session.query(Enrollment).filter(Enrollment.course_id == course_id).all()
+            if enrollment:
+                db.session.delete(enrollment)
+                db.session.commit()
         else:
             raise InputValidationError(404, 'Course not found')
         return "Successfully Deleted", 200
@@ -133,12 +149,11 @@ class CourseApi(Resource):
         course_name = args.get('course_name')
         course_code = args.get('course_code')
         course_description = args.get('course_description')
-        if course_name is None:
+        if course_name is None or course_name.isnumeric():
             raise BusinessValidationError(400, 'COURSE001', 'Course Name is required and should be string.')
-        if course_code is None:
+        if course_code is None or course_code.isnumeric():
             raise BusinessValidationError(400, 'COURSE002', 'Course Code is required and should be string.')
-        # print(type(course_description))
-        if not isinstance(course_description, str):
+        if course_description.isnumeric():
             raise BusinessValidationError(400, 'COURSE003', 'Course Description should be string.')
         user = db.session.query(Course).filter(Course.course_code == course_code).first()
         if user:
@@ -149,13 +164,11 @@ class CourseApi(Resource):
         return new_course, 201
 
 
-# String validation yet to be done
 class StudentApi(Resource):
 
     @marshal_with(student_fields)
     def get(self, student_id):
         user = db.session.query(Student).filter(Student.student_id == student_id).first()
-        # print(user)
         if user:
             return user
         else:
@@ -167,12 +180,11 @@ class StudentApi(Resource):
         first_name = args.get('first_name', None)
         last_name = args.get('last_name', None)
         roll_number = args.get('roll_number', None)
-        if first_name is None:
+        if first_name is None or first_name.isnumeric():
             raise BusinessValidationError(400, 'STUDENT002', 'First Name is required and should be String')
-        if roll_number is None:
+        if roll_number is None or roll_number.isnumeric():
             raise BusinessValidationError(400, 'STUDENT001', 'Roll Number required and should be String')
-        print(not isinstance(last_name, str))
-        if not isinstance(last_name, str):
+        if last_name.isnumeric():
             raise BusinessValidationError(400, 'STUDENT003', 'Last Name is String')
         user_by_student_id = db.session.query(Student).filter(Student.student_id == student_id).first()
         if user_by_student_id is None:
@@ -189,6 +201,10 @@ class StudentApi(Resource):
         if user:
             db.session.delete(user)
             db.session.commit()
+            enrollment = db.session.query(Enrollment).filter(Enrollment.student_id == student_id).all()
+            if enrollment:
+                db.session.delete(enrollment)
+                db.session.commit()
         else:
             raise InputValidationError(404, 'Student not found')
         return "Successfully Deleted", 200
@@ -199,12 +215,11 @@ class StudentApi(Resource):
         first_name = args.get('first_name', None)
         last_name = args.get('last_name', None)
         roll_number = args.get('roll_number', None)
-        if first_name is None:
+        if first_name is None or first_name.isnumeric():
             raise BusinessValidationError(400, 'STUDENT002', 'First Name is required and should be String')
-        if roll_number is None:
+        if roll_number is None or roll_number.isnumeric():
             raise BusinessValidationError(400, 'STUDENT001', 'Roll Number required and should be String')
-        print(not isinstance(last_name, str))
-        if not isinstance(last_name, str):
+        if last_name.isnumeric():
             raise BusinessValidationError(400, 'STUDENT003', 'Last Name is String')
         user = db.session.query(Student).filter(Student.roll_number == roll_number).first()
         if user:
@@ -215,8 +230,61 @@ class StudentApi(Resource):
         return new_student, 201
 
 
+class EnrollmentApi(Resource):
+
+    @marshal_with(enrollment_fields)
+    def get(self, student_id):
+        student = db.session.query(Student).filter(Student.student_id == student_id).first()
+        if student is None:
+            raise BusinessValidationError(400, 'ENROLLMENT002', 'Student does not exist.')
+        user = db.session.query(Enrollment).filter(Enrollment.student_id == student_id).all()
+        if user:
+            return user
+        else:
+            raise InputValidationError(404, 'Student is not enrolled in any course')
+
+    def delete(self, student_id, course_id):
+        student = db.session.query(Student).filter(Student.student_id == student_id).first()
+        if student is None:
+            raise BusinessValidationError(400, 'ENROLLMENT002', 'Student does not exist.')
+        course = db.session.query(Course).filter(Course.course_id == course_id).first()
+        if course is None:
+            raise BusinessValidationError(400, 'ENROLLMENT001', 'Course does not exist')
+        user = db.session.query(Enrollment).filter(Enrollment.student_id == student_id).filter(Enrollment.course_id ==
+                                                                                               course_id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+        else:
+            raise InputValidationError(404, 'Enrollment for the student not found')
+        return "Successfully Deleted", 200
+
+    @marshal_with(enrollment_fields)
+    def post(self, student_id):
+        args = create_enrollment_parser.parse_args()
+        course_id = args.get('course_id')
+        if course_id is None:
+            raise BusinessValidationError(400, 'ENROLLMENT003', 'Course ID is required')
+        student = db.session.query(Student).filter(Student.student_id == student_id).first()
+        if student is None:
+            raise BusinessValidationError(400, 'ENROLLMENT002', 'Student does not exist.')
+        course = db.session.query(Course).filter(Course.course_id == course_id).first()
+        if course is None:
+            raise BusinessValidationError(400, 'ENROLLMENT001', 'Course does not exist')
+        enrollment = db.session.query(Enrollment).filter(Enrollment.student_id == student_id).filter(
+            Enrollment.course_id == course_id).first()
+        if enrollment:
+            raise BusinessValidationError(409, 'ENROLLMENT004', 'Enrollment already exist')
+        new_enrollment = Enrollment(student_id=student_id, course_id=course_id)
+        db.session.add(new_enrollment)
+        db.session.commit()
+        return new_enrollment, 201
+
+
 api.add_resource(CourseApi, '/api/course', '/api/course/<int:course_id>')
 api.add_resource(StudentApi, '/api/student', '/api/student/<int:student_id>')
+api.add_resource(EnrollmentApi, '/api/student/<int:student_id>/course', '/api/student/<int:student_id>/course/<int'
+                                                                        ':course_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
